0c75fc1369687711037854ef5642514b52699c3b
srinivas1901 <88148409+srinivas1901@users.noreply.github.com>
2021-08-04 19:09:17 +0530

    Update file1.tf

--- file1.tf
+++ file1.tf
@@ -4,6 +4,6 @@ access_key = "AKIA3JQCPV4KIZTEM3VI"
 secret_key = "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"
 }
 resource "aws_s3_bucket" "abc" {
-bucket = "akshaysrinivas321"
+bucket = "akshaysrinivas399"
 acl = "private"
 }
